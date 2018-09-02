f2e94ba39b2345c0854b207846f61af8a18e5d9a
Pedro Barata <pedrofelipebarata@gmail.com>
2018-09-02 19:01:25 -0300

    Lecture 101

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 const router = express.Router();
@@ -11,26 +12,63 @@ router.post("/signup", (req, res, next) => {
 
   /* O '10' é "o quão difícil" vai ser a senha, pois utiliza
   algoritmos matemáticos para criar o hash da senha */
-  bcrypt.hash(req.body.password, 10)
-  .then(hash => {
+  bcrypt.hash(req.body.password, 10).then(hash => {
     const user = new User({
       email: req.body.email,
       password: hash
     });
-    user.save()
-    .then(result => {
-      res.status(201).json({
-        message: "User created!",
-        result: result
+    user
+      .save()
+      .then(result => {
+        res.status(201).json({
+          message: "User created!",
+          result: result
+        });
+      })
+      .catch(err => {
+        res.status(500).json({
+          message: err
+        });
       });
-    })
-    .catch(err => {
-      res.status(500).json({
-        message: err
-      });
-    });
   });
 });
 
+router.post("/login", (req, res, next) => {
+  /* Para mexer com o JWT, é necessário instalar um pacote chamado jsonwebtoken */
+  let fetchedUser; /* Para usar o user no segundo then, é necessário passar esse objeto para uma variável externa */
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (!user) {
+        return res.status(404).json({
+          message: "Auth failed"
+        });
+      }
+      fetchedUser = user; //Aqui
+      return bcrypt.compare(req.body.password, user.password);
+    })
+    .then(result => {
+      if (!result) {
+        return res.status(404).json({
+          message: "Auth failed"
+        });
+      }
+      const token = jwt.sign(
+        { email: fetchedUser.email, userId: fetchedUser._id }, //E aqui se usa o fetchedUser ao inves do user
+        "O_secret_deve_ser_o_mais_longo_possivel!",
+        {expiresIn: "1h"} //Nao deve durar muito tempo, pois é bom ficar atualizando o token para o sistema nao ser suscetível a ataques
+      );
+      console.log(token);
+
+      res.status(200).json({
+        token: token
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      return res.status(404).json({
+        message: "Auth failed"
+      });
+    });
+});
 
 module.exports = router;
